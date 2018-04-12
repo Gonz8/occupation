963638b7994ca6a31d71b8efbd24070d8929a418
Dominik Gizynski <dominik764@wp.pl>
2018-04-12 22:07:48 +0200

    metrics

--- scikit.py
+++ scikit.py
@@ -3,6 +3,7 @@ import sys
 
 from sklearn.decomposition import PCA
 from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import mean_squared_error
 
 dt='i3,i3,i3,i3,i3,i3,i3'
 dty='i3'
@@ -27,7 +28,14 @@ pca.fit(X_train)
 X_train = pca.transform(X_train)
 X_test = pca.transform(X_test)
 
-mlp = MLPRegressor(hidden_layer_sizes=(30,30,30), max_iter=500)
+mlp = MLPRegressor(hidden_layer_sizes=(20,30,20), max_iter=5500)
 mlp.fit(X_train, y_train)
 
 predictions = mlp.predict(X_test)
+print(mean_squared_error(y_test,predictions))
+
+mean_val = numpy.mean(y_train)
+y_test_mean = numpy.zeros(y_test.shape[0])
+y_test_mean[:] = mean_val
+
+print(mean_squared_error(y_test_mean,predictions))
